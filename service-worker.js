70ccada408ade62c08a0aa07fa746627660d26e4
mortezakasravi <morteza.kasravi@danfoss.com>
2024-06-24 12:14:52 +0200

    test pwa installation

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,67 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('my-cache').then(cache => {
-            return cache.addAll([
-                '/color-game/',
-                '/color-game/index.html',
-                '/color-game/styles.css',
-                '/color-game/script.js',
-                '/color-game/icon-192x192.png',
-                '/color-game/icon-512x512.png'
-            ]);
-        })
-    );
-});
+const staticCacheName = 'site-static-v2'
+const dynamicCacheName = 'site-dynamic-v1'
+const assets = [
+    '/color-game/',
+    '/color-game/index.html',
+    '/color-game/styles.css',
+    '/color-game/script.js',
+    '/color-game/icon-192x192.png',
+    '/color-game/icon-512x512.png'
+]
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+// cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+      cache.keys().then(keys => {
+        if (keys.length > size) {
+          cache.delete(keys[0]).then(limitCacheSize(name, size))
+        }
+      })
+    })
+  }
+  
+  // install event
+  self.addEventListener('install', evt => {
+    //console.log('service worker installed')
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets')
+        cache.addAll(assets)
+      })
+    )
+  })
+  
+  // activate event
+  self.addEventListener('activate', evt => {
+    //console.log('service worker activated')
+    evt.waitUntil(
+      caches.keys().then(keys => {
+        //console.log(keys)
+        return Promise.all(keys
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+          .map(key => caches.delete(key))
+        )
+      })
+    )
+  })
+  
+  // fetch event
+  self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt)
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone())
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15)
+            return fetchRes
+          })
         })
-    );
-});
+      }).catch(() => {
+        if (evt.request.url.indexOf('.html') > -1) {
+          return caches.match('/color-game/index.html')
+        }
+      })
+    )
+  })
